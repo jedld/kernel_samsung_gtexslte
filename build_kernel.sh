1ec267a03dffcf2f5ab9c969fb77d835110f49f2
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2017-01-16 16:01:52 +0800

    Compile using linaro toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,7 +7,8 @@
 
 set -e -o pipefail
 
-export CROSS_COMPILE=~/android-work/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+# export CROSS_COMPILE=~/android-work/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=/home/jedld/linaro/toolchain/bin/arm-eabi-
 export ARCH=arm
 
 PLATFORM=sc8830
