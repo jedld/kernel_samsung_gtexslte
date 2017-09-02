18ceef44085441b8332768eb4e61b14bfa60467a
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2017-09-02 09:39:35 +0800

    bump to patchset 98
    Change-Id: I8169ffb69293c8757b70c692bf390a502aa06fab

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -2925,7 +2925,7 @@ static int this_tty(const void *t, struct file *file, unsigned fd)
 		return 0;
 	return file_tty(file) != t ? 0 : fd + 1;
 }
-	
+
 /*
  * This implements the "Secure Attention Key" ---  the idea is to
  * prevent trojan horses by killing all processes associated with this
@@ -3630,4 +3630,3 @@ int __init tty_init(void)
 #endif
 	return 0;
 }
-
